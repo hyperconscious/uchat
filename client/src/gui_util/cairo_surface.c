5daf3e6639b4b07ee6b0c4c1aa444db7a481f3b2
Artem Skorik <skorikartem.work@gmail.com>
2024-04-20 13:39:20 +0300

    Add avatars scaling.

--- client/src/gui_util/cairo_surface.c
+++ client/src/gui_util/cairo_surface.c
@@ -4,22 +4,34 @@ gboolean draw_circle_image(GtkWidget *widget,
                            cairo_t *cr,
                            gpointer *user_data) {
     char *image_path = (char *)user_data;
-    cairo_surface_t *image = cairo_image_surface_create_from_png(image_path);
+    GdkPixbuf *pixbuf = gdk_pixbuf_new_from_file(image_path, NULL);
+
     gint widget_width = gtk_widget_get_allocated_width(widget);
     gint widget_height = gtk_widget_get_allocated_height(widget);
     double center_x = widget_width / 2.0;
     double center_y = widget_height / 2.0;
-    double image_x = center_x - cairo_image_surface_get_width(image) / 2.0;
-    double image_y = center_y - cairo_image_surface_get_height(image) / 2.0;
+    double image_width = gdk_pixbuf_get_width(pixbuf);
+    double image_height = gdk_pixbuf_get_height(pixbuf);
+    double scale = MIN(widget_width / image_width,
+                       widget_height / image_height);
+    image_width *= scale;
+    image_height *= scale;
+
+    double image_x = center_x - image_width / 2.0;
+    double image_y = center_y - image_height / 2.0;
     double radius = 0.5 * MIN(widget_width, widget_height);
 
     cairo_new_path(cr);
     cairo_arc(cr, center_x, center_y, radius, 0, 2 * G_PI);
     cairo_close_path(cr);
     cairo_clip(cr);
-    cairo_set_source_surface(cr, image, image_x, image_y);
+    cairo_save(cr);
+    cairo_scale(cr, scale, scale);
+    gdk_cairo_set_source_pixbuf(cr, pixbuf, image_x / scale, image_y / scale);
     cairo_paint(cr);
+    cairo_restore(cr);
     cairo_reset_clip(cr);
+    g_object_unref(pixbuf);
     return FALSE;
 }
 
@@ -27,11 +39,12 @@ gboolean draw_rounded_square_image(GtkWidget *widget,
                                    cairo_t *cr,
                                    gpointer *user_data) {
     char *image_path = (char *)user_data;
-    cairo_surface_t *image = cairo_image_surface_create_from_png(image_path);
+    GdkPixbuf *pixbuf = gdk_pixbuf_new_from_file(image_path, NULL);
+
     gint widget_width = gtk_widget_get_allocated_width(widget);
     gint widget_height = gtk_widget_get_allocated_height(widget);
-    double image_x = widget_width / 2 - cairo_image_surface_get_width(image) / 2.0;
-    double image_y = widget_height / 2 - cairo_image_surface_get_height(image) / 2.0;
+    double image_x = widget_width / 2 - gdk_pixbuf_get_width(pixbuf) / 2.0;
+    double image_y = widget_height / 2 - gdk_pixbuf_get_height(pixbuf) / 2.0;
     double corner_radius = 20.0;
 
     cairo_new_path(cr);
@@ -51,9 +64,9 @@ gboolean draw_rounded_square_image(GtkWidget *widget,
               3 * G_PI / 2);
     cairo_close_path(cr);
     cairo_clip(cr);
-    cairo_set_source_surface(cr, image, image_x, image_y);
+    gdk_cairo_set_source_pixbuf(cr, pixbuf, image_x, image_y);
     cairo_paint(cr);
     cairo_reset_clip(cr);
-    cairo_surface_destroy(image);
+    g_object_unref(pixbuf);
     return FALSE;
 }
