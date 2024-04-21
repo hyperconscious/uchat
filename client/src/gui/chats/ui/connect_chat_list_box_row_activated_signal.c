dedc247dc8064fd972025c36e22393b1867a1bc1
Artem Skorik <skorikartem.work@gmail.com>
2024-04-21 20:12:17 +0300

    Add personal and group chats categories.

--- client/src/gui/chats/ui/connect_chat_list_box_row_activated_signal.c
+++ client/src/gui/chats/ui/connect_chat_list_box_row_activated_signal.c
@@ -1,7 +1,7 @@
 #include "home.h"
 
 void connect_chat_list_box_row_activated_signal(void) {
-    GtkListBox *list_box = get_list_box(CHAT_LIST_BOX_ID);
+    GtkListBox *list_box = get_list_box(PERSONAL_CHAT_LIST_BOX_ID);
     GCallback callback = G_CALLBACK(on_chat_list_item_activated);
     g_signal_connect(list_box, "row-activated", callback, NULL);
 }
--- client/src/gui/chats/ui/set_chat_list_box_row_selected_style.c
+++ client/src/gui/chats/ui/set_chat_list_box_row_selected_style.c
@@ -6,7 +6,7 @@ static void remove_selected_class(GtkListBoxRow *row) {
 }
 
 void set_chat_list_box_row_selected_style(GtkListBoxRow *selected_row) {
-    GtkListBox *chat_list = get_list_box(CHAT_LIST_BOX_ID);
+    GtkListBox *chat_list = get_list_box(PERSONAL_CHAT_LIST_BOX_ID);
     GtkBox *root_box = get_box_from_container(GTK_CONTAINER(selected_row), 0);
     list_box_for_each(chat_list, remove_selected_class);
     gtk_widget_set_name(GTK_WIDGET(root_box), "selected_chat_list_item");
