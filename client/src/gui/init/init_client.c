d385e73aa72965f1c2d53440f68f53e4b2ade51b
Artem Skorik <skorikartem.work@gmail.com>
2024-04-21 21:42:45 +0300

    Small fix.

--- client/src/gui/init/init_client.c
+++ client/src/gui/init/init_client.c
@@ -4,7 +4,7 @@ void init_client(int argc, char **argv) {
     gtk_init(&argc, &argv);
     apply_css_theme();
     set_up_gtk_builder();
-    open_window("home_window");
+    open_window(AUTH_WINDOW_ID);
     gtk_main();
     g_object_unref(Builder);
 }
