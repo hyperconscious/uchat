9df7f4fb72868df5f7e3e121698f419d18110fe8
hyperconscious <kklinaev@gmail.com>
2024-04-24 08:19:45 +0300

    wokr

--- client/src/gui/core/connection_callback.c
+++ client/src/gui/core/connection_callback.c
@@ -1,31 +1,26 @@
 #include "core.h"
 
 gboolean timer_callback() {
-    if(!Client->name) return TRUE;
-    show_internet_connection_status(WAITING);
-  /*  int socket = create_and_connect_socket(serverAddress, Port);
+    int socket = create_and_connect_socket(serverAddress, Port);
     if(socket != -1){
-        show_internet_connection_status(AVAIBLE);
-    printf("1");
-
-
         t_client_rq rq = CONNECTION;
         t_packet request = create_packet(PACKET_TYPE_UINT8, &rq);
-        t_packet heart = create_packet(PACKET_TYPE_UINT32,
-                                      &(uint32_t){3000});
+        t_packet heart = create_packet(PACKET_TYPE_UINT8,
+                                      &(t_client_status_code){SUCCESS});
         send_and_release_packet(socket, &request);
         send_and_release_packet(socket, &heart);
-    printf("1");
         t_packet response;
-        if(!receive_packet(socket, &response))
-        {
-    printf("1");
-            show_internet_connection_status(UNAVAIBLE);
-           pthread_t thread = create_thread(handle_connection_lost, NULL);
-            join_thread(thread, NULL);
+        if(!receive_packet(socket, &response)) {
+            show_internet_connection_status(WAITING);
+            /* pthread_t thread = create_thread(handle_connection_lost, NULL);
+            join_thread(thread, NULL);*/
+        } else {
+            show_internet_connection_status(AVAIBLE);
         }
-    printf("1");
-    }*/
+    } else {
+            show_internet_connection_status(UNAVAIBLE);
+    }
+    close(socket);
     return TRUE;
 }
 
--- server/src/handle_actions/handle_connection.c
+++ server/src/handle_actions/handle_connection.c
@@ -2,9 +2,7 @@
 
 void handle_connection(int client_socket) {
     t_packet heart;
-    printf("1");
     if(!receive_packet(client_socket, &heart)) return;
-        printf("%hu", heart.u_payload.uint8_data);
 
     t_packet response = create_packet(PACKET_TYPE_UINT8,
                                       &(t_client_status_code){SUCCESS});
